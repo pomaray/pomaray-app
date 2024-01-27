ad2e011ebf72559e05e86726ef5450a9d333f344
g3deon <123526476+katia-tsx@users.noreply.github.com>
2024-01-26 22:38:03 -0400

    cambios

--- src/app/tecnicas/[id]/page.tsx
+++ src/app/tecnicas/[id]/page.tsx
@@ -1,45 +1,92 @@
 "use client";
 
 import { useParams } from 'next/navigation';
-import techniquesData, { Technique } from '@/data/techniquesData';
+import tech from '@/locales/tech.json';
+import { Image, Card, CardHeader, CardBody, Button, Chip, Spacer } from "@nextui-org/react";
+import { Section } from '@/components/ui/Section';
 
-const TechniquePage = () => {
-  const { id } = useParams();
+export default function TechniquePage() {
+  const { id } = useParams<{ id: string }>();
 
-  console.log(id)
+  const technique = tech[id as keyof typeof tech];
 
-  const technique: Technique | undefined = techniquesData["informatica"];
+  const handleInformationRequest = () => {
+    const subject = `Solicitud de información - ${technique.TITULO}`;
+    const body = `¡Hola!\n\nMe gustaría obtener más información sobre la técnica de ${technique.TITULO}.\n\nGracias.`;
+    const mailtoLink = `mailto:correo@example.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    window.location.href = mailtoLink;
+  };
 
   if (!technique) {
     return <p>Técnica no encontrada</p>;
   }
 
   return (
-    <div>
-      <h1>{technique.title}</h1>
-      <img src={technique.coverImage} alt={`Cover for ${technique.title}`} />
-      <section>
-        <h2>Perfil</h2>
-        <p>{technique.profile}</p>
-      </section>
-      <section>
-        <h2>¿Por qué esta técnica?</h2>
-        <p>{technique.whyTechnique}</p>
-      </section>
-      <section>
-        <h2>¿Dónde comenzar?</h2>
-        <p>{technique.whereToStart}</p>
-      </section>
-      <section>
-        <h2>¿En qué trabajar?</h2>
-        <p>{technique.whatToWork}</p>
-      </section>
-      <section>
-        <h2>Plan de estudios</h2>
-        <p>{technique.studyPlan}</p>
-      </section>
-    </div>
+    <main className="min-h-screen">
+      <aside className="container mx-auto p-2 flex">
+        <div className=" rounded-lg shadow-lg overflow-hidden flex-grow">
+          <Section className="relative h-90">
+            <Image
+              src={technique.COVER_IMAGE}
+              alt={id.toString()}
+              className="w-full h-full object-cover object-center"
+              classNames={{
+                img: "w-full h-full object-cover !rounded-none",
+                wrapper: "!max-w-full !rounded-none"
+              }}
+            />
+          </Section>
+
+          <Section className="flex items-center">
+            <div className="flex flex-col justify-center items-start p-8 w-full md:w-[80%]">
+              <Chip className="mb-2">{technique.ID}</Chip>
+              <h1 className="text-4xl font-bold mb-2">{technique.TITULO}</h1>
+              <p className="text-gray-700 mb-4">{technique.PROFILE[0]}</p>
+              <Spacer y={2} />
+              <div className="flex space-x-4">
+                <Button color="primary">
+                  Descargar Folleto
+                </Button>
+                <Button onClick={handleInformationRequest} color="primary" variant="bordered">
+                  Solicitar Información
+                </Button>
+              </div>
+            </div>
+          </Section>
+
+          <div className="p-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 gap-4">
+            <section className="mb-8">
+              <h2 className="text-2xl font-bold mb-4">Oportunidades Profesionales</h2>
+              <p className="text-gray-700 mx-auto">{technique.WHAT_TOWORK}</p>
+            </section>
+
+            <section className="mb-8">
+              <h2 className="text-2xl font-bold mb-4">Tu camino de estudio</h2>
+              <p className="text-gray-700 mx-auto">{technique.STUDY_PLAN}</p>
+            </section>
+            
+            </div>
+            <section className="mb-8">
+              <h2 className="text-2xl font-bold mb-4">Galería de Imágenes</h2>
+              <div className="grid grid-cols-12 gap-4">
+                {technique.IMAGENES.map((image, index) => (
+                  <Card key={index} className="col-span-12 sm:col-span-4 h-[300px]">
+                    <CardHeader className="absolute z-10 top-1 flex-col !items-start"></CardHeader>
+                    <Image
+                      removeWrapper
+                      alt={`Image ${index + 1}`}
+                      className="z-0 w-full h-full object-cover"
+                      src={image}
+                    />
+                  </Card>
+                ))}
+              </div>
+            </section>
+          </div>
+        </div>
+      </aside>
+    </main>
   );
 };
 
-export default TechniquePage;
