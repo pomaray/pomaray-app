63a59622e2f5c32e6cc942a1e08ad244e2db746b
g3deon <123526476+katia-tsx@users.noreply.github.com>
2024-01-28 23:00:45 -0400

    Noticias: archivo con las noticias + el diseño renovado y cambios en la parte de nosotros

--- src/app/nosotros/page.tsx
+++ src/app/nosotros/page.tsx
@@ -16,43 +16,39 @@ export default function NosotrosPage() {
 	}
 
 	return (
-		<article className="max-w-5xl mx-auto overflow-hidden px-6 sm:px-12 py-6">
-			<Image
-				src={banner.IMAGEN}
-				alt={banner.titulo}
-				className="rounded-lg" />
-			<Section className="flex flex-col items-center">
+		<article className="max-w-5xl mx-auto overflow-hidden px-6 sm:px-12 py-6 container">
+			<Image src={banner.IMAGEN} alt={banner.titulo} className="rounded-lg" />
+			<div className="flex flex-col items-center">
 
-				<section className="text-foreground sm:py-6 py-2 sm:px-0">
-					<h1 className="sm:text-3xl text-xl font-bold my-4 text-primary">{titulo}</h1>
-					<p className="opacity-80 sm:text-lg text-sm text-pretty sm:mb-4 mb-2">{desarrollo.subtitulo}</p>
+				<section className="sm:py-6 py-2 sm:px-0 ">
+					<h1 className="sm:text-3xl text-xl font-bold my-4 text-primary text-center">{titulo}</h1>
+					<p className="opacity-80 sm:text-lg text-sm text-pretty sm:mb-4 mb-2 text-primary">{desarrollo.subtitulo}</p>
 					{desarrollo.parrafos.map((parrafo: string, index: number) => (
 						<p key={index} className="opacity-80 sm:text-lg text-sm text-pretty sm:mb-4 mb-2">
 							{parrafo}
 						</p>
 					))}
 				</section>
-			</Section>
+			</div>
 
-			<Section className="mb-8">
+			<Section>
 				<h2 className="sm:text-3xl text-xl font-bold my-4 text-primary">{aportesSignificativos.subtitulo}</h2>
 
-				<div className="text-foreground grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
-					<ul className="list-disc ml-6 flex flex-wrap">
+				<article className="text-foreground max-w-[88ch]">
+					<ul className="list-disc ml-6">
 						{aportesSignificativos.contenido.map((aporte: string, index: number) => (
 							<li key={index} className="mb-2 flex-shrink-0">
 								{aporte}
 							</li>
 						))}
 					</ul>
-				</div>
+				</article>
 			</Section>
 
-
 			<Section>
-				<h1 className="text-3xl font-bold mb-4 text-primary">{carrerasPrecursoras.subtitulo}</h1> 
+				<h1 className="text-3xl font-bold mb-4 text-primary">{carrerasPrecursoras.subtitulo}</h1>
 
-				<article className="text-foreground">
+				<article className="text-foreground max-w-[88ch]">
 					<ul className="list-disc ml-6">
 						{carrerasPrecursoras.contenido.map((carrera: string, index: number) => (
 							<li key={index} className="mb-2">
--- src/app/noticias/page.tsx
+++ src/app/noticias/page.tsx
@@ -1,90 +1,103 @@
 "use client"
-import { useRouter } from "next/navigation";
-import tec from "@/locales/noticias.json";
-import { Section, SectionTitle } from "@/components/ui/Section";
+import { Section } from "@/components/ui/Section";
 import {
   Card,
   CardBody,
-  CardFooter,
-  CardHeader,
   Image,
-  Avatar,
   Chip,
+  CardFooter,
 } from "@nextui-org/react";
-import { CiCalendar } from "react-icons/ci";
-import { FaEye } from "react-icons/fa";
+import { useRouter } from "next/navigation";
+import tec from "@/locales/noticias.json";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
-	const router = useRouter();
-	const dynamicPath = "/noticias";
-  
-	return (
-	  <main>
-		<Section className="p-10 text-white">
-		  <Image
-			src="/banner/8.png"
-			alt="Banner"
-			className="w-full h-auto rounded-md shadow-lg"
-		  />
-		</Section>
-  
-		<Section className="py-10 px-10 text-primary">
-		  <SectionTitle withLine text={tec.NOT_TITULO} />
-		  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-y-6 gap-x-46px-6 sm:px-10 md:px-10 lg:px-[1rem]">
-			{tec.DATA.map((noticia) => {
-			  const {
-				ID,
-				imagen_principal,
-				titular,
-				ENTRADA,
-				fecha,
-				vistas,
-				etiqueta,
-			  } = noticia;
-  
-			  return (
-				<Card
-				  key={ID}
-				  onClick={() => {
-					router.push(`${dynamicPath}/${ID.toLowerCase()}`);
-				  }}
-				  isPressable
-				  shadow="lg"
-				  className="hover:transform hover:scale-105 transition-transform relative overflow-hidden w-[40vh]"
-				>
-				  <div className="h-48">
-					<Image
-					  src={imagen_principal}
-					  alt={titular}
-					  className="w-screen h-48 object-cover rounded-t-md"
-					/>
-				  </div>
-  
-				  <CardBody className="text-center py-6">
-					<div className="mb-4">
-					  <Chip color="primary">{etiqueta}</Chip>
-					</div>
-					<h3 className="md:text-md text-lg font-bold mb-2">
-					  {titular}
-					</h3>
-					<p className="text-sm">{ENTRADA}</p>
-				  </CardBody>
-  
-				  <CardFooter className="flex justify-between items-center h-12">
-					<div className="flex items-center space-x-2">
-					  <Avatar icon={<CiCalendar />} />
-					  <span className="text-xs">{fecha}</span>
-					</div>
-					<div className="flex items-center space-x-2">
-					  <Avatar icon={<FaEye />} />
-					  <span className="text-xs">{vistas}</span>
-					</div>
-				  </CardFooter>
-				</Card>
-			  );
-			})}
-		  </div>
-		</Section>
-	  </main>
-	);
-  }
+  const router = useRouter();
+  const dynamicPath = "/noticias";
+
+  const copyToClipboard = () => {
+    const url = window.location.href;
+    navigator.clipboard.writeText(url);
+  };
+
+  const [currentMainNewsIndex, setCurrentMainNewsIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentMainNewsIndex((prevIndex) =>
+        Math.floor(Math.random() * tec.DATA.length)
+      );
+    }, 15000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <main>
+      <section className="mb-8 lg:flex lg:items-center lg:justify-center mx-10	">
+        <article className="lg:w-1/2 h-32 p-2 rounded-md flex items-center py-9 justify-center mb-9 lg:mb-0 lg:mt-6">
+          <Image
+            shadow="none"
+            src={tec.DATA[currentMainNewsIndex].imagen_principal}
+            alt={tec.DATA[currentMainNewsIndex].titular}
+            className="w-screen h-[20vh] lg:w-screen lg:h-[40vh] rounded-md object-cover"
+          />
+        </article>
+        <aside className="lg:w-1/2 p-4 flex flex-col justify-center">
+          <Chip
+            color="primary"
+            variant="bordered"
+            size="lg"
+            className="mb-2 text-ellipsis overflow-hidden"
+          >
+            {tec.DATA[currentMainNewsIndex].etiqueta}
+          </Chip>
+          <h1 className="lg:text-4xl text-2xl font-bold mb-2 text-balance">
+            {tec.DATA[currentMainNewsIndex].titular}
+          </h1>
+          <p className="mb-4 text-sm">
+            {tec.DATA[currentMainNewsIndex].ENTRADA}
+          </p>
+        </aside>
+      </section>
+
+      <section className="p-8 text-primary">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:px-1 md:px-10">
+          {tec.DATA.slice(0, 10).map((noticia, index) => (
+            <Card
+              key={noticia.ID}
+              onClick={() =>
+                router.push(`${dynamicPath}/${noticia.ID.toLowerCase()}`)
+              }
+              isPressable
+              shadow="none"
+              className="relative overflow-hidden w-full max-w-[400px]"
+            >
+              <div className="h-48">
+                <Image
+                  src={noticia.imagen_principal}
+                  alt={noticia.titular}
+                  className="w-screen h-48 object-cover rounded-t-md"
+                />
+              </div>
+
+              <CardBody className="text-center py-4">
+                <Chip color="primary" className="mb-2">
+                  {noticia.etiqueta}
+                </Chip>
+                <h3 className="md:text-md text-lg font-bold mb-2 line-clamp-2">
+                  {noticia.titular}
+                </h3>
+                <p className="text-xs line-clamp-3">{noticia.ENTRADA}</p>
+              </CardBody>
+
+              <CardFooter className="flex justify-between items-center h-10">
+                <time className="mb-2 text-xs ml-2">{noticia.fecha}</time>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+}
