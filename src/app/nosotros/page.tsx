c9298715e0cfa0e9bdf330b95bb8497fd9d8a496
g3deon <123526476+katia-tsx@users.noreply.github.com>
2024-02-24 19:56:03 -0400

    update nosotros

--- src/app/nosotros/page.tsx
+++ src/app/nosotros/page.tsx
@@ -1,66 +1,72 @@
-"use client";
-import i18n from "@/locales/nosotros.json";
-
-import { Divider } from "@nextui-org/divider";
-import { HistoryTimeline } from "@/components/ui/Timeline";
+"use client"
 import { motion } from "framer-motion";
 import Logo from "@/components/ui/Logo";
+import { HistoryTimeline } from "@/components/ui/Timeline";
+import { Divider } from "@nextui-org/divider";
+import i18n from "@/locales/nosotros.json";
 
 export default function NosotrosPage() {
-	return (
-		<motion.article
-			initial={{
-				translateY: 100,
-				opacity: 0,
-			}}
-			animate={{
-				translateY: 0,
-				opacity: 1,
-			}}
-			className="max-w-5xl mx-auto px-6 sm:px-12 py-6"
-		>
-			<section>
-				<div className="flex justify-between gap-4 items-center bg-primary text-white w-full px-2">
-					<h1 className="sm:text-5xl text-4xl font-bold my-4">
-						{i18n.HERO.TITLE}
-					</h1>
-					<Logo className="fill-white mt-2" width={80} />
-				</div>
-				<p className="text-pretty mt-6">{i18n.HERO.SUBTITLE}</p>
-				<div className="mx-auto py-16">
-					<HistoryTimeline points={i18n.TIME_LINE} />
-				</div>
-			</section>
+  return (
+    <motion.article
+      initial={{
+        translateY: 100,
+        opacity: 0,
+      }}
+      animate={{
+        translateY: 0,
+        opacity: 1,
+      }}
+      className="container mx-auto px-6 sm:px-12 py-12 bg-gray-100"
+    >
+      <div className="text-center mb-8">
+        <Logo className="inline-block fill-primary mb-4" width={80} />
+        <h1 className="text-4xl sm:text-5xl font-bold text-primary mb-2">
+          {i18n.HERO.TITLE}
+        </h1>
+        <p className="text-lg text-gray-600">{i18n.HERO.SUBTITLE}</p>
+      </div>
+      
+      <section>
+        <div className="max-w-4xl mx-auto bg-white p-8 rounded-lg shadow-md">
+          <HistoryTimeline points={i18n.TIME_LINE} />
+        </div>
+      </section>
 
-			{i18n.CONTENT.map((sec) => (
-				<section key={sec.TITLE}>
-					<h2 className="sm:text-3xl text-2xl text-balance text-primary font-bold mt-4 mb-2">
-						{sec.TITLE}
-					</h2>
-					<Divider className="bg-primary h-1 rounded-full mb-6	" />
+      {i18n.CONTENT.map((sec) => (
+        <section key={sec.TITLE} className="mt-12">
+          <h2 className="text-3xl sm:text-4xl font-bold text-primary mb-4">
+            {sec.TITLE}
+          </h2>
+          <Divider className="bg-primary h-1 rounded-full mb-6" />
+          <div className="max-w-3xl mx-auto">
+            {sec.PARAGRAPHS.map((parag, paragIndex) => {
+              const isList = parag.substring(0, 2).trim() === "-";
+              const isLastList =
+                sec.PARAGRAPHS[paragIndex - 1] &&
+                sec.PARAGRAPHS[paragIndex - 1].substring(0, 2).trim();
 
-					{sec.PARAGRAPHS.map((parag, paragIndex) => {
-						const isList = parag.substring(0, 2).trim() === "-";
-						const isLastList =
-							sec.PARAGRAPHS[paragIndex - 1] &&
-							sec.PARAGRAPHS[paragIndex - 1].substring(0, 2).trim();
+              if (isList) {
+                return (
+                  <li className="py-2 list-disc" key={parag}>
+                    {parag.replace("-", "")}
+                  </li>
+                );
+              }
 
-						if (isList) {
-							return (
-								<li className="py-2" key={parag}>
-									{parag.replace("-", "")}
-								</li>
-							);
-						}
-
-						return (
-							<p className={`text-pretty ${isLastList && "pt-4"}`} key={parag}>
-								{parag}
-							</p>
-						);
-					})}
-				</section>
-			))}
-		</motion.article>
-	);
+              return (
+                <p
+                  className={`text-lg text-gray-700 ${
+                    isLastList && "pt-4"
+                  }`}
+                  key={parag}
+                >
+                  {parag}
+                </p>
+              );
+            })}
+          </div>
+        </section>
+      ))}
+    </motion.article>
+  );
 }
